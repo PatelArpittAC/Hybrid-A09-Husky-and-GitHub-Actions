338380b9de4555d339f55ca8fdc67ba5a5660071
PatelArpittAC <pate1096@algonquinlive.com>
2024-03-09 00:03:06 -0500

    ADDED: initial terraform commit

---
+++ infrastructure/storage.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features {}
+
+
+# Data source to fetch information about an existing Azure resource group
+data "azurerm_resource_group" "existing" {
+  name = "CST8918-DEMO" # Name of the existing resource group
+
+
+# Resource block to create an Azure Storage Account
+resource "azurerm_storage_account" "example" {
+  name                     = "arpitstorageaccount"
+  resource_group_name      = data.azurerm_resource_group.existing.name # Reference to the existing resource group
+  location                 = "canadacentral"                          # Location where the storage account will be created
+  account_tier             = "Standard"
+  account_replication_type = LRS
+
+
+# Output block to display the name of the created storage account
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name
+
